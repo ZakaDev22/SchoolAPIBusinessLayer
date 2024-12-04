f58afebc9e52d74284a64dac895108d548205f15
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-12-04 01:40:04 +0100

    Finishing Students Parents Data Access Methods Add Get All Mehtod In Logic Layer Add Get All In The API Layer (Test Get All Done Successfully.)

--- clsStudentParents.cs
+++ clsStudentParents.cs
@@ -1,12 +1,39 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SchoolAPiDataAccessLayer;
 
 namespace SchoolBusinessLayer
 {
-    internal class clsStudentParents
+    public class clsStudentParents
     {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode _Mode = enMode.AddNew;
+
+        public studentParentDTO sClassDTO
+        {
+            get
+            {
+
+                return new studentParentDTO(this.ID, this.StudentID, this.ParentID);
+            }
+        }
+
+        public int ID { get; set; }
+        public int StudentID { get; set; }
+        public int ParentID { get; set; }
+
+        public clsStudentParents(studentParentDTO studentParentDTO, enMode mode)
+        {
+            _Mode = mode;
+            ID = studentParentDTO.ID;
+            StudentID = studentParentDTO.StudentID;
+            ParentID = studentParentDTO.ParentID;
+        }
+
+        public static async Task<IEnumerable<studentParentDTO>> GetAllAsync()
+        {
+            var list = await clsStudentParentsData.GetAllAsync();
+
+
+            return list == null ? Enumerable.Empty<studentParentDTO>() : list;
+        }
     }
 }

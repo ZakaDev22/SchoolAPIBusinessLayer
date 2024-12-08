53c319ca73687a7c48779246cac511e1c4cb6aa5
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-12-08 23:17:18 +0100

    Fixing Stored Procedurs Parameters In Sql Server ✅ Add Two Methods In Three Layers (Get All - Get By ID) ✅ Test With Swagger : Success ✅

--- clsPhoneNumbers.cs
+++ clsPhoneNumbers.cs
@@ -1,12 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SchoolAPiDataAccessLayer;
 
 namespace SchoolBusinessLayer
 {
-    internal class clsPhoneNumbers
+    public class clsPhoneNumbers
     {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+        public phoneNumberDTO phoneNumberDTO
+        {
+            get
+            {
+                return new phoneNumberDTO(this.ID, this.Number, this.PersonID, this.PhoneTypeID, this.IsPrimary);
+            }
+        }
+
+        public int ID { get; set; }
+        public string Number { get; set; }
+        public int PersonID { get; set; }
+        public int PhoneTypeID { get; set; }
+        public bool IsPrimary { get; set; }
+
+        public clsPhoneNumbers(phoneNumberDTO number, enMode mode)
+        {
+            Mode = mode;
+            ID = number.ID;
+            Number = number.Number;
+            PersonID = number.PersonID;
+            PhoneTypeID = number.PhoneTypeID;
+            IsPrimary = number.IsPrimary;
+        }
+
+        public static async Task<IEnumerable<phoneNumberDTO>> GetAllAsync()
+        {
+            var list = await clsPhoneNumbersData.GetAllAsync();
+
+            return list == null ? Enumerable.Empty<phoneNumberDTO>() : list;
+        }
+
+        public static async Task<clsPhoneNumbers> GetByIDAsync(int ID)
+        {
+            var number = await clsPhoneNumbersData.GetByIdAsync(ID);
+
+            return number != null ? new clsPhoneNumbers(number, enMode.Update) : null;
+        }
     }
 }

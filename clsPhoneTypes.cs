84163ddcee81987196828819285fc386b3fb160d
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-12-13 23:11:05 +0100

    Add Two Phone Types Methods In All layers (Get All - Get By ID) - Swagger Tests : Success ✅

--- clsPhoneTypes.cs
+++ clsPhoneTypes.cs
@@ -1,12 +1,42 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SchoolAPiDataAccessLayer;
 
 namespace SchoolBusinessLayer
 {
-    internal class clsPhoneTypes
+    public class clsPhoneTypes
     {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+        public phoneTypesDTO phoneTypeDTO
+        {
+            get
+            {
+                return new phoneTypesDTO(this.ID, this.Name);
+            }
+        }
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public clsPhoneTypes(phoneTypesDTO phoneTypeDTO, enMode mode)
+        {
+            Mode = mode;
+            ID = phoneTypeDTO.ID;
+            Name = phoneTypeDTO.Name;
+        }
+
+        public static async Task<IEnumerable<phoneTypesDTO>> GetAllAsync()
+        {
+            var list = await clsPhoneTypesData.GetAllAsync();
+
+            return list == null ? Enumerable.Empty<phoneTypesDTO>() : list;
+        }
+
+        public static async Task<clsPhoneTypes> GetByIDAsync(int ID)
+        {
+            var phoneType = await clsPhoneTypesData.GetByIdAsync(ID);
+
+            return phoneType != null ? new clsPhoneTypes(phoneType, enMode.Update) : null;
+        }
     }
 }

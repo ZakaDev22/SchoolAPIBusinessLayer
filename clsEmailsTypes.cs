4b1dccfe28dd0db4c966bee8724afa1de75c7962
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-12-13 21:35:14 +0100

    Finishing Email Types Data Access

--- clsEmailsTypes.cs
+++ clsEmailsTypes.cs
@@ -1,12 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SchoolAPiDataAccessLayer;
 
 namespace SchoolBusinessLayer
 {
-    internal class clsEmailsTypes
+    public class clsEmailsTypes
     {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+        public emailTypesDTO emailTypeDTO
+        {
+            get
+            {
+                return new emailTypesDTO(this.ID, this.Name);
+            }
+        }
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+
     }
 }

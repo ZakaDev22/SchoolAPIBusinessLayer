44d947af09f1a32ec1841ce0fac0381bc9f068f2
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-12-28 21:19:48 +0100

    Add relationships Types ( Get All And Get By ID) In Three Layers

--- clsRelationshipTypes.cs
+++ clsRelationshipTypes.cs
@@ -1,12 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SchoolAPiDataAccessLayer;
 
 namespace SchoolBusinessLayer
 {
-    internal class clsRelationshipTypes
+    public class clsRelationshipTypes
     {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+        public RelationshipTypeDTO relationshipTypeDTO
+        {
+            get
+            {
+                return new RelationshipTypeDTO(this.ID, this.Name);
+            }
+        }
+
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public clsRelationshipTypes(RelationshipTypeDTO relTypeDTO, enMode mode)
+        {
+            Mode = mode;
+            ID = relTypeDTO.ID;
+            Name = relTypeDTO.Name;
+        }
+
+        public static async Task<IEnumerable<RelationshipTypeDTO>> GetAllAsync()
+        {
+            var list = await clsRelationshipTypesData.GetAllAsync();
+
+            return list == null ? Enumerable.Empty<RelationshipTypeDTO>() : list;
+        }
+
+        public static async Task<clsRelationshipTypes> GetByIDAsync(int ID)
+        {
+            var relationType = await clsRelationshipTypesData.GetByIdAsync(ID);
+
+            return relationType != null ? new clsRelationshipTypes(relationType, enMode.Update) : null;
+        }
+
     }
 }

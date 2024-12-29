a36bd36b4c3fbd7adaa9fc9715bbdbd3798d6fae
ZakaDev22 <elfakharzakaria22@gmail.com>
2024-12-30 00:10:09 +0100

    Add States Method In the three Layers (Get All - Get All By Country ID - Get By ID - Get By Name) Swagger Test : Success For All Projects Section 1 Done Successfully.

--- clsStates.cs
+++ clsStates.cs
@@ -1,12 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using SchoolAPiDataAccessLayer;
 
 namespace SchoolBusinessLayer
 {
-    internal class clsStates
+    public class clsStates
     {
+        public enum enMode { AddNew = 0, Update = 1 };
+        public enMode Mode = enMode.AddNew;
+
+        public stateDTO stateDTO
+        {
+            get
+            {
+                return new stateDTO(this.ID, this.CountryID, this.Name);
+            }
+        }
+
+        public int ID { get; set; }
+        public int CountryID { get; set; }
+        public string Name { get; set; }
+
+        public clsStates(stateDTO stateDTO, enMode mode)
+        {
+            Mode = mode;
+            ID = stateDTO.ID;
+            CountryID = stateDTO.CountryID;
+            Name = stateDTO.Name;
+        }
+
+        public static async Task<IEnumerable<stateDTO>> GetAllAsync()
+        {
+            var list = await clsStatesData.GetAllAsync();
+
+            return list == null ? Enumerable.Empty<stateDTO>() : list;
+        }
+        public static async Task<IEnumerable<stateDTO>> GetAllStatsByCountryIDAsync(int ID)
+        {
+            var list = await clsStatesData.GetAllStatesByCountryIDAsync(ID);
+
+            return list == null ? Enumerable.Empty<stateDTO>() : list;
+        }
+
+        public static async Task<clsStates> GetByIDAsync(int ID)
+        {
+            var state = await clsStatesData.GetByIdAsync(ID);
+
+            return state != null ? new clsStates(state, enMode.Update) : null;
+        }
+
+        public static async Task<clsStates> GetByStateNameAsync(string Name)
+        {
+            var state = await clsStatesData.GetByStateNameAsync(Name);
+
+            return state != null ? new clsStates(state, enMode.Update) : null;
+        }
     }
 }
